c698d973f93bd8d4d985a11ea143a73035ed60ab
Javier Carreño <javiercm.desarrollador@gmail.com>
2022-07-27 16:25:52 -0300

    Map Added

---
+++ Map.tsx
@@ -0,0 +1,39 @@
+// REVISADO +
+import { Fragment } from 'react';
+import { ToastContainer } from 'react-toastify';
+import { selectCoordsIds, selectPlacesIds, selectViewCoordCoordsResult } from '../redux/selectors';
+import { useAppSelector, useMap } from '../hooks';
+import { CoordsResult, LocationButton, PlacesResult, SearchInput, ViewCoordButtons } from '../components';
+import { MapContainer } from '../styles/Map';
+import 'react-toastify/dist/ReactToastify.css';
+
+const Map = () => {
+    const { mapContainer } = useMap();
+
+    const coordsIds = useAppSelector(selectCoordsIds);
+    const placesIds = useAppSelector(selectPlacesIds);
+    const { show: showViewCoord } = useAppSelector(selectViewCoordCoordsResult);
+
+    return (
+        <Fragment>
+            <ToastContainer />
+
+            <MapContainer
+                ref={mapContainer}
+                className="animate__animated animate__fadeIn"
+            />
+
+            {(showViewCoord) && <ViewCoordButtons />}
+
+            {(!showViewCoord && coordsIds.length > 0) && <CoordsResult />}
+
+            {(!showViewCoord && coordsIds.length <= 0) && <LocationButton />}
+
+            {(!showViewCoord && coordsIds.length <= 0) && <SearchInput />}
+
+            {(!showViewCoord && coordsIds.length <= 0 && placesIds.length > 0) && <PlacesResult />}
+        </Fragment>
+    );
+};
+
+export default Map;
